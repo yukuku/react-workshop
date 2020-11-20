5cd6a6e262b8e8351a37a6cad4896a4543e45eae
Brad Westfall <brad@azpixels.com>
2020-11-19 21:51:16 -0700

    end of day one

--- courses/core/01-thinking-in-react/lecture/index.js
+++ courses/core/01-thinking-in-react/lecture/index.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { MdShoppingCart } from 'react-icons/md'
+import { MdShoppingCart } from 'react-icons/md'
 import 'YesterTech/styles/global-styles.scss'
 import './styles.scss'
 
-// Let's go
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  )
+}
+
+function App() {
+  function handleClick() {
+    console.log('Add to cart stuff')
+  }
+  return (
+    <div>
+      <Button onClick={handleClick}>
+        <MdShoppingCart color="blue" />
+        <span>Add to Shopping Cart</span>
+        <MdShoppingCart color="blue" />
+      </Button>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- courses/core/02-state/lecture/Quantity.js
+++ courses/core/02-state/lecture/Quantity.js
@@ -1,26 +1,56 @@
 import React, { useState } from 'react'
-// import ReactDOM from 'react-dom'
 import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
 
 export default function Quantity() {
-  const quantity = 1
+  const [quantity, setQuantity] = useState(0)
+  const [error, setError] = useState()
 
-  return (
+  function subtract() {
+    const nextQuantity = quantity - 1
+    setQuantity(nextQuantity)
+    if (nextQuantity < 0) {
+      setError('Cannot be less than 0')
+    }
+  }
+
+  function add() {
+    setQuantity(quantity + 1)
+  }
+
+  const output = (
     <div className="quantity-picker">
       <div>
+        <SomeComp></SomeComp>
         <div>
-          <button type="button" className="icon-button">
+          <button onClick={subtract} type="button" className="icon-button">
             <FaMinusCircle />
           </button>
         </div>
         <div className="input-container">{quantity}</div>
         <div>
-          <button type="button" className="icon-button">
+          <button onClick={add} type="button" className="icon-button">
             <FaPlusCircle />
           </button>
         </div>
       </div>
+      <p>{error}</p>
     </div>
   )
+  console.log(output)
+  return output
+}
+
+function SomeComp() {
+  console.log('child rerender')
+  return (
+    <div id="somecomp">
+      <Other></Other>
+    </div>
+  )
+}
+
+function Other() {
+  console.log('other rerender')
+  return <div id="other"></div>
 }
