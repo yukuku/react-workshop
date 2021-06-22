33e5d960df797e42db119e83318476a54ede7bf3
Chance Strickland <chance.strickland@gmail.com>
2021-06-22 13:57:28 -0700

    forms lecture

--- courses/core-v1/03-forms/lecture/Quantity.tsx
+++ courses/core-v1/03-forms/lecture/Quantity.tsx
@@ -3,21 +3,54 @@ import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
 
 function Quantity() {
-  const quantity = 1
+  const [quantity, setQuantity] = React.useState(1)
+
+  function add() {
+    setQuantity((quantity) => quantity + 1)
+  }
+
+  function subtract() {
+    setQuantity((quantity) => {
+      if (quantity >= 1) {
+        return quantity - 1
+      }
+      return quantity
+    })
+  }
 
   return (
     <div className="quantity-picker">
       <div>
         <div>
-          <button type="button" className="icon-button" aria-label="Remove an item">
+          <button
+            type="button"
+            className="icon-button"
+            aria-label="Remove an item"
+            onClick={subtract}
+          >
             <FaMinusCircle />
           </button>
         </div>
         <div className="input-container">
-          <input type="text" aria-label="quantity" defaultValue={quantity} />
+          <input
+            type="text"
+            aria-label="quantity"
+            value={quantity}
+            onChange={(event) => {
+              let input = event.target;
+              let value = input.value
+              let numberValue = parseInt(value, 10)
+
+              if (typeof numberValue === 'number' && !isNaN(numberValue)) {
+                setQuantity(numberValue)
+              } else if (value === '') {
+                setQuantity(0)
+              }
+            }}
+          />
         </div>
         <div>
-          <button type="button" className="icon-button" aria-label="Add an item">
+          <button type="button" className="icon-button" aria-label="Add an item" onClick={add}>
             <FaPlusCircle />
           </button>
         </div>
