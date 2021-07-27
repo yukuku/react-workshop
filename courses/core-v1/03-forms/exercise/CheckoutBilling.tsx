a2a7deb522e312ea2629ec79d5dd3e352dd4ca1a
Chance Strickland <chance.strickland@gmail.com>
2021-07-27 15:07:06 -0700

    lecture on forms

--- courses/core-v1/03-forms/exercise/CheckoutBilling.tsx
+++ courses/core-v1/03-forms/exercise/CheckoutBilling.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { MdShoppingCart } from 'react-icons/md'
-import serializeForm from 'form-serialize'
 import Heading from 'YesterTech/Heading'
 
 interface CheckoutBillingProps {
@@ -12,16 +11,40 @@ type TextFieldName = 'billingName' | 'billingAddress' | 'shippingName' | 'shippi
 type TextFields = Record<TextFieldName, string>
 
 const CheckoutBilling: React.FC<CheckoutBillingProps> = ({ onSubmit }) => {
-  const [sameAsBilling, setSameAsBilling] = React.useState(false)
+  const [formValues, setFormValues] = React.useState({
+    sameAsBilling: false,
+    billingName: '',
+    billingAddress: '',
+    shippingName: '',
+    shippingAddress: '',
+  })
+  const { sameAsBilling, billingName, billingAddress, shippingAddress, shippingName } = formValues
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     // When the fields are stored in state above, this fields variable can just be
     // an object filled with the field states. We don't need `serializeForm` anymore
-    const fields = serializeForm(event.target as HTMLFormElement, {
-      hash: true,
-    })
+    const fields = {
+      billingName,
+      billingAddress,
+      shippingName: sameAsBilling ? billingName : shippingName,
+      shippingAddress: sameAsBilling ? billingAddress : shippingAddress,
+    }
     onSubmit(sameAsBilling, fields)
+  }
+
+  function setFormField(name, value) {
+    setFormValues((formValues) => ({
+      ...formValues,
+      [name]: value,
+    }));
+    // setFormValues((prevValue) => {
+    //   let newValue = {
+    //     ...prevValue,
+    //     [name]: value,
+    //   }
+    //   return newValue
+    // })
   }
 
   return (
@@ -36,11 +59,31 @@ const CheckoutBilling: React.FC<CheckoutBillingProps> = ({ onSubmit }) => {
         <hr />
         <div className="form-field">
           <label htmlFor="billing:name">Name</label>
-          <input id="billing:name" type="text" required name="billingName" autoComplete="off" />
+          <input
+            id="billing:name"
+            type="text"
+            required
+            name="billingName"
+            autoComplete="off"
+            value={billingName}
+            onChange={(event) => {
+              setFormField('billingName', event.target.value)
+            }}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="billing:address">Address</label>
-          <input id="billing:address" type="text" required name="billingAddress" />
+          <input
+            id="billing:address"
+            type="text"
+            required
+            name="billingAddress"
+            value={billingAddress}
+            onChange={(event) => {
+              setFormField('billingAddress', event.target.value)
+            }}
+            autoComplete="off"
+          />
         </div>
 
         <Heading as="h2" size={3}>
@@ -50,15 +93,30 @@ const CheckoutBilling: React.FC<CheckoutBillingProps> = ({ onSubmit }) => {
         <label>
           <input
             type="checkbox"
-            defaultChecked={sameAsBilling}
-            onChange={() => setSameAsBilling(!sameAsBilling)}
+            checked={sameAsBilling}
+            onChange={(event) => {
+              setFormField('sameAsBilling', event.target.checked)
+            }}
           />{' '}
           Same as Billing
         </label>
 
         <div className="form-field">
           <label htmlFor="shipping:name">Name</label>
-          <input id="shipping:name" type="text" required name="shippingName" autoComplete="off" />
+          <input
+            id="shipping:name"
+            type="text"
+            required
+            name="shippingName"
+            autoComplete="off"
+            disabled={sameAsBilling || undefined}
+            value={sameAsBilling ? billingName : shippingName}
+            onChange={(event) => {
+              if (!sameAsBilling) {
+                setFormField('shippingName', event.target.value)
+              }
+            }}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="shipping:address">Address</label>
@@ -68,6 +126,13 @@ const CheckoutBilling: React.FC<CheckoutBillingProps> = ({ onSubmit }) => {
             required
             name="shippingAddress"
             autoComplete="off"
+            disabled={sameAsBilling || undefined}
+            value={sameAsBilling ? billingAddress : shippingAddress}
+            onChange={(event) => {
+              if (!sameAsBilling) {
+                setFormField('shippingAddress', event.target.value)
+              }
+            }}
           />
         </div>
 
