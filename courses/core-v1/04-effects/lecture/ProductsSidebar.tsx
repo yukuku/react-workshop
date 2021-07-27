d68369f50904682ec165dbf240bf5c78bcb4e939
Chance Strickland <chance.strickland@gmail.com>
2021-07-27 16:06:04 -0700

    lecture 4

--- courses/core-v1/04-effects/lecture/ProductsSidebar.tsx
+++ courses/core-v1/04-effects/lecture/ProductsSidebar.tsx
@@ -1,12 +1,39 @@
 import * as React from 'react'
 import ProductFilters from 'YesterTech/ProductFilters'
 
-function ProductsSidebar() {
-  return (
+let count = 0
+
+/*
+React.useEffect(() => {}); // sync this effect with ALL changes
+React.useEffect(() => {}, []); // sync this effect with NO changes
+React.useEffect(() => {}, [sync, with, these]);
+ */
+
+function ProductsSidebar({ minWidth = 600 }) {
+  let mql = React.useMemo(() => {
+    return window.matchMedia(`(min-width: ${minWidth}px)`)
+  }, [minWidth])
+
+  let [matches, setMatches] = React.useState(mql.matches)
+
+  React.useEffect(() => {
+    console.log('effect callback fired')
+    let listener = (event) => {
+      setMatches(event.matches)
+    }
+
+    mql.addEventListener('change', listener)
+    return function cleanup() {
+      console.log('cleanup  fired')
+      mql.removeEventListener('change', listener)
+    }
+  }, [mql])
+
+  return matches ? (
     <aside>
       <ProductFilters />
     </aside>
-  )
+  ) : null
 }
 
 export default ProductsSidebar
