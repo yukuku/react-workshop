c0379d7d0c343efb14fcf903ddd7c8c1bf8975c1
Brad Westfall <brad@azpixels.com>
2022-03-28 23:05:42 -0400

    end of day one

--- courses/core/01-thinking-in-react/lecture/index.jsx
+++ courses/core/01-thinking-in-react/lecture/index.jsx
@@ -1,8 +1,43 @@
-// import React from 'react' <-- We don't need this anymore (React 17)
 import ReactDOM from 'react-dom'
 import { FaTrash } from 'react-icons/fa'
 import './styles.scss'
 
-// This is our "entry" file for our build system.
-// Let's start making our React app and "mount" it
-// to the DOM
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick} className="button" type="button">
+      {children}
+    </button>
+  )
+}
+
+// When react calls App() that is a "render"
+function App() {
+  const courses = [
+    { id: 1, name: 'HTML' },
+    { id: 2, name: 'CSS' },
+    { id: 3, name: 'JS' },
+  ]
+
+  function onClick(e) {
+    console.log('logic for removing a course')
+  }
+
+  return (
+    <div>
+      {courses.map((course) => {
+        return (
+          <div key={course.id}>
+            <h1>{course.name}</h1>
+            <Button onClick={onClick}>
+              <FaTrash />
+              <span>Remove Course</span>
+            </Button>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
--- courses/core/02-state-and-refs/lecture/Counter.tsx
+++ courses/core/02-state-and-refs/lecture/Counter.tsx
@@ -1,28 +1,72 @@
 import { useState } from 'react'
 import { Icon } from 'course-platform/Icon'
 
-// type CounterProps = {
-//   count: number
-//   setCount(count: number): void
-//   min?: number
+// const states = []
+// let count = -1
+
+// function useState(defaultState) {
+//   const callId = ++count
+
+//   if (states[callId]) {
+//     return states[callId]
+//   }
+
+//   function setState(newState) {
+//     states[callId][0] = newState
+//     count = -1
+//     reRender()
+//   }
+
+//   const state = [defaultState, setState]
+//   states[callId] = state // same as a push
+//   return state
 // }
 
-export function Counter() {
-  let count = 0
+// function reRender() {
+// }
+
+type CounterProps = {
+  count: number
+  setCount(count: number): void
+  min?: number
+}
+
+export function Counter({ count, setCount, min = 0 }: CounterProps) {
+  const [error, setError] = useState<string | null>(null)
+
+  function subtract() {
+    setCount(count - 1)
+    if (count - 1 < 0) {
+      setError('Cannot be less than 0')
+    }
+  }
+
+  function add() {
+    setCount(count + 1)
+  }
 
   return (
-    <div className="counter inline-flex flex-gap">
-      <div>
-        <button className="button button-small">
-          <Icon name="minus" />
-        </button>
+    <>
+      <div className="counter inline-flex flex-gap">
+        <div>
+          <button onClick={subtract} className="button button-small">
+            <Icon name="minus" />
+          </button>
+        </div>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => {
+            setCount(parseInt(e.target.value))
+          }}
+        />
+        <div>
+          <button onClick={add} className="button button-small">
+            <Icon name="plus" />
+          </button>
+        </div>
       </div>
-      <div className="input">{count}</div>
-      <div>
-        <button className="button button-small">
-          <Icon name="plus" />
-        </button>
-      </div>
-    </div>
+      {error && <p>{error}</p>}
+    </>
   )
 }
