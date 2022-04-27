7532ea869311c6e07ebe75b3c9fa1de882b6432e
Yuku on yuku10 <yukuku@gmail.com>
2022-04-27 14:17:55 +0800

    Advanced exercise 3 chat

--- courses/advanced-hooks/03-chat-subscription/exercise/ChatPage.tsx
+++ courses/advanced-hooks/03-chat-subscription/exercise/ChatPage.tsx
@@ -17,27 +17,71 @@ export function ChatPage() {
 
   // You'll use Date.now() for this state (which is a number)
   const [startSubscription, setStartSubscription] = useState<number | null>(null)
+  const inputText = useRef<HTMLInputElement>(null!)
 
   // Get initial messages
-  // api.chat.getMessages(THREAD_NAME).then((messages) => {
-  // })
+  useEffect(() => {
+    api.chat.getMessages(THREAD_NAME).then((messages) => {
+      setMessages(messages)
+      setStartSubscription(Date.now())
+    })
+  }, [])
+
+  // scroll to bottom
+  useEffect(() => {
+    if (scrolledToBottom) {
+      chatBoardRef.current.scrollTop = chatBoardRef.current.scrollHeight
+    }
+  }, [messages])
 
   // Subscribe to new messages
-  // const cleanup = api.chat.subscribe(
-  //   Date.now(),
-  //   (newMessages) => {
-  //   },
-  //   THREAD_NAME
-  // )
+  useEffect(() => {
+    if (startSubscription != null) {
+      const cleanup = api.chat.subscribe(
+        startSubscription,
+        (newMessages) => {
+          console.log('old messages', messages.length, 'new messages', newMessages.length)
+          setMessages([...messages, ...newMessages])
+
+          // resubscribe
+          let now
+          if (newMessages.length > 0) {
+            now = newMessages[newMessages.length - 1].created
+          } else {
+            now = Date.now()
+          }
+          setStartSubscription(now)
+        },
+        THREAD_NAME
+      )
+      return () => cleanup();
+    }
+  }, [startSubscription]);
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
     if (!user || !input) return
+    console.log('me too')
+    setInput('')
+    inputText.current.focus()
     api.chat.postMessage(input, user.id, user.name, user.avatarUrl, THREAD_NAME).then(() => {
       // noop (no operation)
+      console.log('submitted')
       // We don't need to do anything when we post a message here to get it added to state. The
       // subscription will do that for us
     })
+  }
+
+  function spam() {
+    if (!user) return
+    for (let i = 0; i < 100; i++) {
+      api.chat.postMessage(`load tester ${i}`, user.id, user.name, user.avatarUrl, THREAD_NAME).then(() => {
+        // noop (no operation)
+        console.log('submitted')
+        // We don't need to do anything when we post a message here to get it added to state. The
+        // subscription will do that for us
+      })
+    }
   }
 
   function onBoardScroll(event: any) {
@@ -70,10 +114,13 @@ export function ChatPage() {
           })}
       </div>
       <form onSubmit={onSubmit} className="chat-controls spacing">
-        <input className="form-field" value={input} onChange={(e) => setInput(e.target.value)} />
+        <input ref={inputText} className="form-field" value={input} onChange={(e) => setInput(e.target.value)} />
         <div>
           <button type="submit" className="button">
             Send
+          </button>
+          <button type="button" className="button" onClick={spam}>
+            spam
           </button>
         </div>
       </form>
